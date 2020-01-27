19700d70ce72daddf58506d732ab5a030462b9ef
Imelda <60111288+imeldapv@users.noreply.github.com>
2020-01-27 12:29:52 -0700

    Practica terminada

---
+++ clase_1/PracticaUno/PracticaUno/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PracticaUno
+{
+    class Program
+    {
+        // Variable de la clase
+        static string nombre = "Imelda";
+        static void Main(string[] args)
+        {
+            // Respondemos con el primer argumento y segundo
+            // Condicion swe debe de cumplir todas las sentencias si utilizamos &&
+            // Condicion swe debe de cumplir cualquierda de las sentencias si utilizamos ||
+            // Tipos de comparacion: < > == <= >= !=
+            if (args[0].Length > 0 && args [0] == nombre) // verdadero o falso
+            {
+                Console.WriteLine($"Hello {args[0]}");
+            }else
+            {
+                Console.WriteLine("User unauthorized");
+            }
+        }
+    }
+}
