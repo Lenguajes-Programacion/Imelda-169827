2a2224dd4f2def681cc8950c9c8e68ac4f17db05
Imelda <60111288+imeldapv@users.noreply.github.com>
2020-03-11 11:53:29 -0600

    Arreglos Multidimensionales
    Arreglos Multidimensionales

--- PracticaDos/PracticaDos/Memoria.cs
+++ PracticaDos/PracticaDos/Memoria.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
@@ -9,70 +9,160 @@ namespace PracticaDos
 {
     class Memoria
     {
+        // Creación de una lista nativa para el manejo de la memoria.
+        public List<MemoriaData> db = new List<MemoriaData>();
+        public Memoria()
+        {
+            // Constructor de la clase Memoria, para inicializar nuestra lista
+            db = new List<MemoriaData>();
+        }
         public void LeerMemoria()
         {
+            ConsoleColor currentColor = Console.BackgroundColor;
             string archivoDB = "../../../db.json";
-            using (var reader = new StreamReader(archivoDB))
+            StreamReader reader = new StreamReader(archivoDB);
+            var dbJSON = reader.ReadToEnd();
+            var dbObject = JObject.Parse(dbJSON);
+            // Prueba de lectura de archivo db.json
+            //var result = dbObject.ToString();
+            //var result = dbObject["arreglo"].ToString();
+            //var result = dbObject["arreglo"][0].ToString();
+            // Lectura de json iterable
+            int i = 0; // Indice creado manualmente para ubicar facilmente nuestros objetos dentro de la lista db.
+            foreach (var item  in dbObject)
             {
-                var dbJSON = reader.ReadToEnd();
-                var dbObject = JObject.Parse(dbJSON);
-                // Prueba de lectura de archivo db.json
-                // var result = dbObject.ToString();
-                // var result = dbObject["arreglo"].ToString();
-                // var result = dbObject["arreglo"][0].ToString();
-                // Lectura del json iterable
-                foreach (var item in dbObject)
+                // Iteración individual de cada grupo de datos del objeto json.
+                MemoriaData memoriaData = new MemoriaData(item.Key.ToString(), item.Value["operacion"].ToString(),item.Value["resultado"].ToString());
+                this.db.Add(memoriaData);
+                Console.WriteLine("Dato en memoria: ({0})", i);
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("{0} - {1}", memoriaData.fecha.ToLongDateString(),
+                memoriaData.fecha.ToLongTimeString());
+                Console.ResetColor();
+                Console.WriteLine("Operación: {0}",memoriaData.operacion);
+                Console.WriteLine("Resultado: {0}",memoriaData.resultado.ToString());
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("----------------- \n");
+                Console.ForegroundColor = ConsoleColor.White;
+                i++;
+            }
+        }
+        public int GetMemoriaData(String key)
+        {
+            // Encontrar el dato deseado con indice manual en el parseo del json.
+            int index = int.Parse(key);
+            // Opción Nativa:  para buscar de manera nativa en nuestro List db. Con el indice autogenerado.
+            // En tres lineas de código encontramos y reutilizamos el valor que se busca.
+            MemoriaData data = db[index];
+            return data.resultado;
+        }
+        public void GuardarMemoria(MemoriaData data)
+        {
+            db.Add(data);
+            int i = 0;
+            db.ForEach((MemoriaData memoriaData) =>
+            {
+                Console.WriteLine("Dato en memoria: ({0})", i);
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("{0} - {1}", memoriaData.fecha.ToLongDateString(),
+                memoriaData.fecha.ToLongTimeString());
+                Console.ResetColor();
+                Console.WriteLine("Operación: {0}", memoriaData.operacion);
+                Console.WriteLine("Resultado: {0}", memoriaData.resultado.ToString());
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("----------------- \n");
+                Console.ForegroundColor = ConsoleColor.White;
+                i++;
+            });
+            string json = JsonConvert.SerializeObject(db.ToArray(), Formatting.Indented);
+            string archivoDB = "../../../db.json";
+            File.WriteAllText(archivoDB, json);
+        }
+        public void arreglo()
+        {
+            string[] Colores = { "Rojo", "Blanco", "Morado" };
+            //List<string> colores = ["Rojo", "Blanco", "Morado"];
+            //colores.Sort();
+            Array.Reverse(Colores);
+            Array.ForEach(Colores, (item)=>{
+                Console.WriteLine(item);
+            });
+            String color = Array.Find(Colores, (item) => {
+                return item.Length > 4;
+            });
+            Console.WriteLine(color);
+            Console.WriteLine("Accede tus colores y separalos con comas(,):");
+            String colorUser = Console.ReadLine();
+            // Un string se puede convertir en arreglo con su propiedad Split, dándole el patron.
+            string[] newColors = colorUser.Split(' ');
+            Console.WriteLine(newColors);
+        }
+
+        //public void multidimensional()
+        static void Main(String [] args)
+        {
+            bool salir =  false;
+            while (!salir)
+            {
+                Console.WriteLine ("Arreglo sensillo");
+                string[] sencillo = { "Rojo", "Blanco", "Morado" };
+                Console.WriteLine ("[{0}]", string.Join(",", sencillo));
+                Console.WriteLine("Arreglo dos dimensiones");
+                int[,] dosDimensiones = new int[5,5];
+                for (int i = 0; i < dosDimensiones.GetLength(0); i++)
                 {
-                    // Iteración individual de cada grupo de datos del objeto json.
-                    Console.WriteLine("Dato en memoria:");
-                    MemoriaData memoriaData = new MemoriaData(DateTime.Now, item.Value["operacion"].ToString(), (int)item.Value["resultado"]);
-                    Console.WriteLine("Fecha \n:");
-                    Console.WriteLine(item.Key.ToString());
-                    Console.WriteLine("El resultado es \n:");
-                    Console.WriteLine(memoriaData.resultado.ToString());
+                    for (int j = 0; j < dosDimensiones.GetLength(1);j++)
+                    {
+                        Console.WriteLine("{0}, {1} = {2}", i,j, dosDimensiones[i,j]);
+                    }
+                }
+                Console.WriteLine("Arreglo de tres dimensiones");
+                int [,,] tresDimensiones = new int[5, 5, 5];
+                for (int i = 0; i < tresDimensiones.GetLength(0); i++)
+                {
+                    for (int j = 0; j < tresDimensiones.GetLength(1); j++)
+                    {
+                        for (int k = 0; k < tresDimensiones.GetLength(2); k++)
+                        {
+
+                            Console.WriteLine("{0}, {1}, {2} = {3}", i,j,k, tresDimensiones[i,j,k]);
+                        }
+                    }
+                }
+                Console.WriteLine("Arreglo de cuatro dimensiones");
+                int [,,,] cuatroDimensiones = new int[5, 5, 5, 5];
+                for (int i = 0; i < cuatroDimensiones.GetLength(0); i++)
+                {
+                    for (int j = 0; j < cuatroDimensiones.GetLength(1); j++)
+                    {
+                        for (int k = 0; k < cuatroDimensiones.GetLength(2); k++)
+                        {
+                            for (int l = 0; l < cuatroDimensiones.GetLength(2); l++)
+                            {
+                                Console.WriteLine("{0}, {1}, {2}, {3} = {4}", i,j,k,l, cuatroDimensiones[i,j,k,l]);
+                            }
+                        }
+                    }
+                }
+                string exit = Console.ReadLine();
+                if (exit == "y")
+                {
+                    salir = true;
                 }
             }
         }
     }
-
-    public void arreglos ()
-    {
-        string[] Colores = {"Rojo", "Blanco", "Morado"};
-        //List<string> colores = ["Rojo", "Blanco", "Morado"];
-        //colores.Sort();
-        Array.Sort(Colores);
-        Array.ForEach(Colores, (item)=>{
-            Console.WriteLine(item);
-        });
-        String color =Array.Find(Colores, (item) => {
-            return item.Length > 4;
-            //return item != "Morado";
-        });
-        
-        Console.WriteLine(color);
-        Console.WriteLine ("Accede tus colores favoritos y separalos por espacios:");
-        String colorUser = Console.ReadLine();
-        //Un string se puede convertir en arreglo con su propedad Split, dandole el patron.
-        string[] newColors = colorUser.Split(' ');
-        Console.WriteLine(newColors);
-    }
-        public void multidimensional ()
-        {
-            int[,] array = new int [4, 2,3];
-            Console.WriteLine(array);
-        }
-
     class MemoriaData
     {
         public DateTime fecha;
         public String operacion;
         public int resultado;
 
-        public MemoriaData(DateTime date, String operation, int result)
+        public MemoriaData( String date, String operation, String result)
         {
-            fecha = date;
+            fecha = DateTime.Parse(date);
             operacion = operation;
-            resultado = result;
+            resultado = int.Parse(result);
         }
     }
 
--- PracticaDos/PracticaDos/Program.cs
+++ PracticaDos/PracticaDos/Program.cs
@@ -2,16 +2,18 @@
 
 namespace PracticaDos
 {
-    class Program
+    class Calculadora
     {
-        static void Main(string[] args)
+        //static void Main(string[] args)
+        static void Calcu()
         {
+            Memoria memoria = new Memoria();
             Console.WriteLine("CalculApp");
             Console.WriteLine("----------------- \n");
             bool close_app = true;
             int valor1 = 0;
             int valor2 = 0;
-            float result = 0;
+            int result = 0;
             while (close_app)
             {
                 Console.WriteLine("Elige una opción:");
@@ -19,7 +21,8 @@ namespace PracticaDos
                 Console.WriteLine("(-) Resta");
                 Console.WriteLine("(*) Multiplicación");
                 Console.WriteLine("(/) Disión");
-                Console.WriteLine("(m) memoria");
+                Console.WriteLine("(m) Memoria");
+                Console.WriteLine("(mr) Borrar Memoria");
                 Console.WriteLine("e Para salir \n");
                 string opcion = Console.ReadLine();
                 switch (opcion)
@@ -27,50 +30,67 @@ namespace PracticaDos
                     case "+":
                         Console.WriteLine("Suma \n");
                         Console.WriteLine("Captura el primer valor:");
-                        valor1 = int.Parse(Console.ReadLine());
+                        valor1 = result == 0 ? int.Parse(Console.ReadLine()) : result;
                         Console.WriteLine("Captura el segundo valor:");
                         valor2 = int.Parse(Console.ReadLine());
                         result = Calculus.suma(valor1, valor2);
                         Console.WriteLine("Tu resultado es: {0} \n", result);
+                        String o = valor1 + "+" + valor2;
+                        MemoriaData data = new MemoriaData(DateTime.Now.ToString(), o, result.ToString());
+                        memoria.GuardarMemoria(data);
+                        result = 0;
                         break;
                     case "-":
                         Console.WriteLine("Resta \n");
                         Console.WriteLine("Captura el primer valor:");
-                        valor1 = int.Parse(Console.ReadLine());
+                        valor1 = result == 0 ? int.Parse(Console.ReadLine()) : result;
                         Console.WriteLine("Captura el segundo valor:");
                         valor2 = int.Parse(Console.ReadLine());
                         result = Calculus.resta(valor1, valor2);
                         Console.WriteLine("Tu resultado es: {0} \n", result);
+                        result = 0;
+
                         break;
                     case "*":
                         Console.WriteLine("Multiplicación \n");
                         Console.WriteLine("Captura el primer valor:");
-                        valor1 = int.Parse(Console.ReadLine());
+                        valor1 = result == 0 ? int.Parse(Console.ReadLine()) : result;
                         Console.WriteLine("Captura el segundo valor:");
                         valor2 = int.Parse(Console.ReadLine());
                         result = Calculus.multi(valor1, valor2);
                         Console.WriteLine("Tu resultado es: {0} \n", result);
+                        result = 0;
+
                         break;
                     case "/":
                         Console.WriteLine("División \n");
                         Console.WriteLine("Captura el primer valor:");
-                        valor1 = int.Parse(Console.ReadLine());
+                        valor1 = result == 0 ? int.Parse(Console.ReadLine()) : result;
                         Console.WriteLine("Captura el segundo valor:");
                         valor2 = int.Parse(Console.ReadLine());
                         result = Calculus.division(valor1, valor2);
                         Console.WriteLine("Tu resultado es: {0} \n", result);
+                        result = 0;
+
                         break;
                     case "m":
                         Console.WriteLine("Accediendo a Memoria \n");
                         Console.WriteLine("-----------------\n");
-                        memoria.arreglo();
-                        Memoria memoria = new Memoria();
-                        memoria.LeerMemoria();
-                        // Elegir la opcion de resultado en nuestra memoria y reautilizarla en alguna operacion.
-                        // 1. Crear metodo que nos regrese el resultado en una nueva operacion.
-                        // 2. Utilizar el resultado en una nueva operacion.
-                        // 3. Guardar la nueva operacion en nuestro db.Json
-
+                        //memoria.multidimensional();
+                        String key = Console.ReadLine();
+                        //Console.WriteLine(data.ToString());
+                        // Elegir la opcion de resultado en nuestra memoria 
+                        // y reutilizarla en alguna operación.
+                        // 1. Crear metodo que nos regrese el resultado seleccionado.
+                        result = memoria.GetMemoriaData(key);
+                        // 2. Utilizar el resultado en una nueva operación.
+                        Console.WriteLine(result);
+                        // 3. Guardar la nueva operación en nuestra db.json.
+                        // memoria.GuardarMemoria();
+                        break;
+                    case "mr":
+                        Console.WriteLine("Memoria Temporal Reiniciada! \n");
+                        result = 0;
                         break;
                     case "e":
                         Console.WriteLine("-----------------\n");
